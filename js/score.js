528fcc94a7f0d8be2f379d070780b71223f020ad
Rocketpowered-tech <chaserob12345@gmail.com>
2026-03-11 19:47:05 -0400

    Refactor score calculation formula in score.js

--- js/score.js
+++ js/score.js
@@ -24,8 +24,8 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = math.round((500 / ((rank + 6.5) / 7.5)) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1))));
+    let score = (500 / ((rank + 6.5) / 7.5)) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
 
